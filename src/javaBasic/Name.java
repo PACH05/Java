03f5004bcdab692141eb63196e73f6737c9f2372
Prasanta <acharyya.prasanta05@gmail.com>
2021-11-25 09:58:48 +0530

    first commit

--- src/javaBasic/Name.java
+++ src/javaBasic/Name.java
@@ -2,12 +2,12 @@ package javaBasic;
 
 import java.util.*;
 
+//Basic I/O operations in Java
 public class Name {
 
     public static void main(String args[]) {
         System.out.println("Hello");
         String st;
-
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your name : ");
         st = sc.next();
