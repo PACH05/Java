64fdc2eb2208f0a9125c12e732acaa349d99f0e6
Prasanta <acharyya.prasanta05@gmail.com>
2022-01-03 23:29:17 +0530

    first commit

--- src/javaDSA/ClimbingStairs.java
+++ src/javaDSA/ClimbingStairs.java
@@ -1,17 +1,17 @@
 package javaDSA;
 
-
+//Leetcode question
 //Stairs can be crossed either by crossing 1 at a time or 2 at a time
 public class ClimbingStairs {
             public static void main(String[] args) {
                 int n=5;              //Number of stairs        
-
                 int steps[] = new int[n];
                 if(n<=0)
                     System.out.println("0");
                 if(n==1)
                     System.out.println("1");
                 else{
+                    //This is a Dynamic Programming approach
                 steps[0]=1;
                 steps[1]=2;
                 for (int i = 2; i <=n-1; i++) {
