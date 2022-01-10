915dab533c0a42333293835dba22926331891f91
Prasanta <acharyya.prasanta05@gmail.com>
2022-01-10 18:24:10 +0530

    first commit

--- src/javaDSA/BubbleSort.java
+++ src/javaDSA/BubbleSort.java
@@ -1,26 +1,26 @@
 package javaDSA;
 
 public class BubbleSort {
-    public static void main(String[] args) {
-      int arr[]={1,5,8,4,9,2,3};
-      
-      for (int i = 0; i < arr.length-1; i++) {
+  public static void main(String[] args) {
+    int arr[] = { 1, 5, 8, 4, 9, 2, 3 };
 
-          for (int j = 0; j < arr.length-1; j++) {
+    for (int i = 0; i < arr.length - 1; i++) {
 
-            if (arr[j]>arr[j+1]) {
-                // int temp = arr[j];
-                // arr[j]=arr[j+1];
-                // arr[j+1]=temp;
-                arr[j]^=arr[j+1];
-                arr[j+1]^=arr[j];
-                arr[j]^=arr[j+1];
-              }
-          }
-      }
-      for (int i : arr) {
-          System.out.println(i);
-          
+      for (int j = 0; j < arr.length - 1; j++) {
+
+        if (arr[j] > arr[j + 1]) {
+          // int temp = arr[j];
+          // arr[j]=arr[j+1];
+          // arr[j+1]=temp;
+          arr[j] ^= arr[j + 1];
+          arr[j + 1] ^= arr[j];
+          arr[j] ^= arr[j + 1];
+        }
       }
     }
+    for (int i : arr) {
+      System.out.println(i);
+
+    }
+  }
 }
