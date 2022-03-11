139d5220c1e48006c1f27208f47d6e9c7ff82079
Prasanta <acharyya.prasanta05@gmail.com>
2022-03-11 09:04:45 +0530

    first commit

---
+++ src/javaDSA/ReverseLL.java
@@ -0,0 +1,47 @@
+package javaDSA;
+
+public class ReverseLL {
+    public static void main(String[] args) {
+        Node node = new Node(1);
+        node.next = new Node(2);
+        node.next.next = new Node(3);
+        node.next.next.next = new Node(4);
+        printLL(node);
+        node = reversell(node);
+        System.out.println("\n");
+        printLL(node);
+    }
+    
+    public static Node reversell(Node head){
+        Node curr = head;
+        Node prev=null;
+        Node next=null;
+        while (curr!=null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+
+    }
+    public static void printLL(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.print("NULL");
+    }
+    // For Singly Linked Lists
+    static class Node {
+        int data;
+        Node next;
+    
+        public Node(int data) {
+            this.data = data;
+                this.next = null;
+            }
+}
+}
