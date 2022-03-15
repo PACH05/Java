8fa57aa5d61abfd1a50e8e35dac07fa98bbd1157
Prasanta <acharyya.prasanta05@gmail.com>
2022-03-15 22:41:52 +0530

    initial commit

---
+++ JavaNew/javaprograms/Test.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        float a;
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextInt();
+        System.out.println(a);
+        sc.close();
+    }      
+}
