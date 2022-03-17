078b9556b847acdb5b0be097c58383a121b17d44
Prasanta <acharyya.prasanta05@gmail.com>
2022-03-17 09:37:02 +0530

    initial commit

---
+++ src/javaBasic_and_OOP/Lambda.java
@@ -0,0 +1,20 @@
+package javaBasic_and_OOP;
+
+import java.util.function.Function;
+//This program shows the use of lambda functions in Java using generics 
+public class Lambda {
+    public static void main(String[] args) {
+        Function<Integer, Integer> abc = (x) -> {return x*x;};
+        System.out.println("The square of 10 is : "+ abc.apply(10));
+    }
+}
+
+// interface Example{
+//     public void example1();
+// }
+
+// int a = 10;
+// Example ex = ()-> { 
+//     System.out.println("Example of Lambda Method : "+ a);
+// };
+// ex.example1();
