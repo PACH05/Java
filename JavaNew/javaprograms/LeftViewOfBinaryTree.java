45ff2c5d5a7f40934487280f1992802843b80bc2
Prasanta Acharyya <acharyya.prasanta05@gmail.com>
2022-04-09 14:25:42 +0530

    Code Added

--- JavaNew/javaprograms/LeftViewOfBinaryTree.java
+++ JavaNew/javaprograms/LeftViewOfBinaryTree.java
@@ -5,8 +5,8 @@ public class LeftViewOfBinaryTree {
 
     public static void main(String[] args) {
         Node root = new Node(1);
-        root.right = new Node(2);
-        root.left = new Node(3);
+        root.right = new Node(3);
+        root.left = new Node(2);
         root.left.left = new Node(4);
         root.right.left = new Node(6);
         root.left.right = new Node(5);
