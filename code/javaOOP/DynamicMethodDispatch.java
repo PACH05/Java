1acea0345788a61e25eeb2178ff833573d6afb68
Prasanta Acharyya <acharyya.prasanta05@gmail.com>
2022-04-22 10:54:36 +0530

    code added

---
+++ code/javaOOP/DynamicMethodDispatch.java
@@ -0,0 +1,32 @@
+package javaOOP;
+
+public class DynamicMethodDispatch {
+  public static void main(String[] args) {
+      A a = new A();
+      B b = new B();
+      C c = new C();
+      A r;
+      r=a;
+      r.print();
+      r=b;
+      r.print();
+      r=c;
+      r.print();
+  }   
+}
+
+class A {
+    void print(){
+        System.out.println("Class A");
+    }
+}
+class B extends A{
+    void print(){
+        System.out.println("Class B");
+    }
+}
+class C extends A {
+    void print(){
+        System.out.println("Class C");
+    }
+}
